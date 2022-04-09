3be9f71b1baf2fb13b8e38c3594dc3cd5a41824e
Daniel Navarro <dnava039@fiu.edu>
2022-04-08 23:56:45 -0400

    Adding styling and responsive text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <body class = "bg-yellow-400">
+      
+      <h1 className="text-3xl md:text-6xl lg:text-7xl font-bold underline text-blue-700">
+        Personal Portfolio
+      </h1>
+    
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+  Button
+</button>
+</body>
+
+    </div>
+   
+    
   );
 }
 
