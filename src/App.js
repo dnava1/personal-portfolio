eb8fb563623a38ac0d7bca91f67af97c3972f798
Daniel Navarro <dnava039@fiu.edu>
2022-04-11 16:14:36 -0400

    Added heart beating animation

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import postIcon from "./Images/postIcon.png"
 import npmIcon from "./Images/npmIcon.png"
 import profilepicture from "./Images/profilepicture.png"
 import educationIcon from "./Images/educationIcon.png"
+import heart from "./Images/heart.svg"
 
 
 
@@ -74,6 +75,7 @@ function App() {
       </h3>
       
       <img src={profilepicture} height={300} width={300} class="pt-10"/>
+      
 
     
 
@@ -85,6 +87,7 @@ function App() {
         window.open("https://github.com/dnava1");
       }}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+        
         </button>
 
         <button class="hover:animate-pulse  px-4 mt-1" 
@@ -93,6 +96,7 @@ function App() {
         window.open("https://www.linkedin.com/in/danielnav/");
       }}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+        
         
         </button>
         
@@ -107,7 +111,7 @@ function App() {
         and solving the problems that come along. My hobbies include hiking, gardening, and cooking.
       </h1>
 
-      <button class="rounded-full hover:animate-bounce button text-white bg-gradient-to-r from-pink-700 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg font-mediumtext-sm px-3 py-2.5 text-center "
+      <button class="rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 button text-white bg-gradient-to-r from-pink-700 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg font-mediumtext-sm px-3 py-2.5 text-center "
         
         onClick={(e) => {
         e.preventDefault();
@@ -349,6 +353,8 @@ fsdfsdfs
             
         </div>
 
+        
+
           <div class="bg-neutral-900 rounded-md py-4 px-4 border-2 border-pink-700 hover:opacity-70 cursor-pointer">
           <a onClick={(e) => {
        e.preventDefault();
@@ -356,6 +362,12 @@ fsdfsdfs
             }}>
             
               <h1 class="text-lg text-white mb-3 font-semibold">Soft Skills</h1>
+
+              <h1 class="grid place-items-center animate-beat">
+        <img src={heart} height={35} width={35} class="pt-10 pb-20"/>
+
+        </h1>
+
               
               <p class="loading-relaxed text-sm mb-3 text-white">•Detail Oriented</p>
               <p class="loading-relaxed text-sm mb-3 text-white">•Verbal and Written Communication</p>
@@ -365,14 +377,20 @@ fsdfsdfs
              
           </div>
 
+        
+
          
     
 
         </div>
-
+        
 
       </div>
 
+     
+
+      
+    
       
 
       
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}","./node_modules/flowbite/**/*.js"], 
   
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        beat: "beat 1s ease-in-out infinite",
+      },
+      keyframes:{
+        beat:{
+          '0%, 100%': {transform: 'scale(1)'},
+          '50%': {transform: 'scale(1.5)'}
+        }
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
