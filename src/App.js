47a5aa13b26bedc51407a18098814868b90c51dc
Daniel Navarro <dnava039@fiu.edu>
2022-04-11 01:57:39 -0400

    More style improvements

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
               
               <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
                 <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
-                  <li><a href="#Home" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Home</a></li>
+                  
                   <li><a href="#About" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">About</a></li>
                   <li><a href="#Skills" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Skills</a></li>
                   <li><a href="#Projects" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Projects</a></li>
@@ -55,21 +55,23 @@ function App() {
       </nav>
 
 
-      <div class="grid place-items-center">
-      <h1 id="About" className="text-3xl md:text-6xl lg:text-7xl underline font-extrabold text-blue-700">
-        Personal Portfolio
-      </h1>
-      <h2 className = "text-2xl md:text-4xl lg:text-6xl font-extrabold text-blue-700">
+      <div id="About" class="grid place-items-center">
+      <h1  className="text-4xl md:text-6xl lg:text-8xl underline font-bold text-blue-700">
         Daniel Navarro
+      </h1>
+      <h2 className = "text-2xl md:text-4xl lg:text-6xl font-semibold text-blue-700">
+        Software Engineer
       </h2>
-      <h3 className = "text-1xl md:text-2xl lg:text-3xl font-extrabold text-pink-400 mx-auto">
-        I am a motivated and enthusiastic software developer completing my BS in Computer Science in Spring 2022. I enjoy building interesting software
-        and solving the problems that come alongside that. 
+      <h3 className = "text-1xl md:text-2xl lg:text-3xl font-semibold text-blue-700">
+        Miami, Fl • 786-609-7234 • dnava039@fiu.edu
       </h3>
+      
+
+    
 
       <div>
 
-      <button type="button" class="py-2 px-4 ml-4 mt-4" 
+      <button type="button" class="hover:animate-pulse px-4 mt-1 mb-2" 
       onClick={(e) => {
         e.preventDefault();
         window.open("https://github.com/dnava1");
@@ -77,7 +79,7 @@ function App() {
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
         </button>
 
-        <button class=" py-2 px-4 ml-4 mt-4" 
+        <button class="hover:animate-pulse  px-4 mt-1" 
         onClick={(e) => {
         e.preventDefault();
         window.open("https://www.linkedin.com/in/danielnav/");
@@ -88,7 +90,7 @@ function App() {
         
         </div>
 
-        <button class="hover:animate-bounce button text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        <button class="rounded-full hover:animate-bounce button text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-mediumtext-sm px-3 py-2.5 text-center "
         
         onClick={(e) => {
         e.preventDefault();
@@ -101,9 +103,14 @@ function App() {
 
         </div>
 
+        <h1 className = "text-center text-1xl md:text-2xl lg:text-3xl font-extrabold text-pink-400 mx-auto py-2 px-2 max-w-screen-lg mt-10">
+        I am a motivated and enthusiastic software developer completing my BS in Computer Science in Spring 2022. I enjoy building interesting software
+        and solving the problems that come along.
+      </h1>
+
         
 
-        <h1 className = "text-2xl md:text-4xl lg:text-6xl font-extrabold text-blue-700 grid place-items-center">
+        <h1 className = "text-2xl md:text-4xl lg:text-6xl font-bold text-blue-700 grid place-items-center">
         About
       </h1>
       <p className ="grid place-content-center">
@@ -111,7 +118,7 @@ fsdfsdfs
       </p>
       
 
-        <h1 id="Skills" className = "text-2xl md:text-4xl lg:text-6xl font-extrabold text-blue-700 grid place-items-center">
+        <h1 id="Skills" className = "text-2xl md:text-4xl lg:text-6xl font-bold text-blue-700 grid place-items-center">
         Skills
       </h1>
 
@@ -144,35 +151,35 @@ fsdfsdfs
 
           
 
-          <div class="bg-white rounded-md py-4 px-4 border-2 border-pink-400">
-           
-              <h1 class="text-lg text-silver mb-3 font-semibold">Frameworks</h1>
-              <p class="loading-relaxed text-sm mb-3 columns-3">
-              <img src={reactIcon}height={85} width={85} />
-              <img src={tailwindIcon}height={85} width={85} />
-              <img src={djangoIcon}height={85} width={85} />
-              
-              
-              </p>
-              
-          </div>
+         
 
           <div class="bg-white rounded-md py-4 px-4 border-2 border-pink-400">
-          
               <h1 class="text-lg text-silver mb-3 font-semibold">Tools</h1>
               <p class="loading-relaxed text-sm mb-3 columns-3">
               <img src={gitIcon}height={85} width={85} />
               <img src={androidIcon}height={85} width={85} />
               <img src={puttyIcon}height={85} width={85} />
-              <img src={winscpIcon}height={85} width={85} />
               <img src={postIcon}height={85} width={85} />
-             
               <img src={vsIcon}height={85} width={85} />
+              <img src={winscpIcon}height={85} width={85} />
               <img src={githubIcon}height={85} width={85} />
               <img src={npmIcon}height={85} width={85} />
               </p>
               
           </div>
+
+          <div class="bg-white rounded-md py-4 px-4 border-2 border-pink-400">
+           
+           <h1 class="text-lg text-silver mb-3 font-semibold">Frameworks</h1>
+           <p class="loading-relaxed text-sm mb-3 columns-3">
+           <img src={reactIcon}height={85} width={85} />
+           <img src={tailwindIcon}height={85} width={85} />
+           <img src={djangoIcon}height={85} width={85} />
+           
+           
+           </p>
+           
+       </div>
 
           <div class="bg-white rounded-md py-4 px-4 border-2 border-pink-400">
            
@@ -188,6 +195,16 @@ fsdfsdfs
 
           <div class="bg-white rounded-md py-4 px-4 border-2 border-pink-400">
             
+            <h1 class="text-lg text-silver mb-3 font-semibold">Concepts</h1>
+            <p class="loading-relaxed text-sm mb-3">•Agile Methodoloy</p>
+            <p class="loading-relaxed text-sm mb-3">•Object-Oriented Programming</p>
+            <p class="loading-relaxed text-sm mb-3">•RESTful APIs</p>
+            <p class="loading-relaxed text-sm mb-3">•Data Structures and Algorithms</p>
+            
+        </div>
+
+          <div class="bg-white rounded-md py-4 px-4 border-2 border-pink-400">
+            
               <h1 class="text-lg text-silver mb-3 font-semibold">Soft Skills</h1>
               
               <p class="loading-relaxed text-sm mb-3">•Detail Oriented</p>
@@ -197,14 +214,7 @@ fsdfsdfs
              
           </div>
 
-          <div class="bg-white rounded-md py-4 px-4 border-2 border-pink-400">
-            
-              <h1 class="text-lg text-silver mb-3 font-semibold">Concepts</h1>
-              <p class="loading-relaxed text-sm mb-3">•Agile Methodoloy</p>
-              <p class="loading-relaxed text-sm mb-3">•Object-Oriented Programming</p>
-              <p class="loading-relaxed text-sm mb-3">•RESTful APIs</p>
-              
-          </div>
+         
     
 
         </div>
