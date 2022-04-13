569ebd43e8b60ea8f9b973c4198fa99f03d6049a
Daniel Navarro <dnava039@fiu.edu>
2022-04-12 23:55:09 -0400

    Added more content to education sections

--- src/App.js
+++ src/App.js
@@ -136,15 +136,22 @@ fsdfsdfs
         
 
       </h1>
-      <h2 className ="grid place-content-center text-white">
-      <img src={educationIcon} />
-         B.S. - Computer Science
 
-      </h2>
-      <p className ="grid place-content-center text-white">
-        Florida International University - Miami, FL
+       <h2 class ="grid place-content-center text-white">
+      <img src={educationIcon} />
+      🎓B.S. - Computer Science
+
+      
+      <p class ="grid place-content-center text-white ml-5">
+      •Florida International University - Miami, FL
+        <p>•Aug 2019 - May 2022</p>
+        •GPA - 3.81
+        <p > •Upsilon Pi Epsilon (UPE) </p>
 
       </p>
+      </h2>
+      <p class="grid place-content-center text-white mt-3">hello</p>
+     
       
 
         <h1 id="Skills" className = "text-2xl md:text-4xl lg:text-6xl font-bold text-blue-700 grid place-items-center">
@@ -278,16 +285,12 @@ fsdfsdfs
           <div class="bg-neutral-900 rounded-md py-4 px-4 border-2 border-pink-700 ">
             
             
-              <h1 class="text-lg text-white mb-3 font-semibold ">Languages</h1>
+              <h1 class="text-lg text-white mb-3 font-semibold ">Dijkstra Algorithm Visualizer</h1>
               <p class="loading-relaxed text-sm mb-3 columns-3">
               <img src={javaIcon} height={85} width={85}  />
              
               <img src={htmlIcon} height={85} width={85} />
-              <img src={cssIcon} height={85} width={85} />
-              <img src={javascriptIcon} height={85} width={85} />
-              <img src={pythonIcon} height={85} width={85} />
-              <img src={cIcon}height={85} width={85} />
-              <img src={sqlIcon}height={85} width={85} />
+            
               
               </p>
               
@@ -299,23 +302,18 @@ fsdfsdfs
          
 
           <div class="bg-neutral-900 rounded-md py-4 px-4 border-2 border-pink-700">
-              <h1 class="text-lg text-white mb-3 font-semibold">Tools</h1>
+              <h1 class="text-lg text-white mb-3 font-semibold">Plant Tracker Website</h1>
               <p class="loading-relaxed text-sm mb-3 columns-3">
               <img src={gitIcon}height={85} width={85} />
               <img src={androidIcon}height={85} width={85} />
-              <img src={puttyIcon}height={85} width={85} />
-              <img src={postIcon}height={85} width={85} />
-              <img src={vsIcon}height={85} width={85} />
-              <img src={winscpIcon}height={85} width={85} />
-              <img src={githubIcon}height={85} width={85} />
-              <img src={npmIcon}height={85} width={85} />
+              
               </p>
               
           </div>
 
           <div class="bg-neutral-900 rounded-md py-4 px-4 border-2 border-pink-700">
            
-           <h1 class="text-lg text-white mb-3 font-semibold">Frameworks</h1>
+           <h1 class="text-lg text-white mb-3 font-semibold">Bookstore Backend</h1>
            <p class="loading-relaxed text-sm mb-3 columns-3">
            <img src={reactIcon}height={85} width={85} />
            <img src={tailwindIcon}height={85} width={85} />
@@ -328,7 +326,7 @@ fsdfsdfs
 
           <div class="bg-neutral-900 rounded-md py-4 px-4 border-2 border-pink-700">
            
-              <h1 class="text-lg text-white mb-3 font-semibold">Databases</h1>
+              <h1 class="text-lg text-white mb-3 font-semibold">Stress and Anxiety Android App</h1>
               <p class="loading-relaxed text-sm mb-3 columns-3">
               <img src={mongoIcon}height={85} width={85} />
               <img src={firebaseIcon}height={85} width={85} />
@@ -398,19 +396,14 @@ fsdfsdfs
             <a href="#" class="mr-4 hover:underline md:mr-6 text-white ">Resume</a>
         </li>
         <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 text-white">Github</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 text-white">LinkedIn</a>
+        </li>
+        <li>
             <a href="#" class="hover:underline text-white">Contact</a>
         </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
-            </a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
-            </a>
-        </li>
-       
     </ul>
 </footer>
 
